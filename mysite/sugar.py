92b4e3ed48e66f5e915601389203f5b9937e3992
jashingden <jashingden@gmail.com>
2023-08-25 17:40:50 +0800

    update sugar.py

--- mysite/sugar.py
+++ mysite/sugar.py
@@ -5,82 +5,81 @@ Created on Tue Mar  9 17:22:07 2021
 @author: eddyteng
 """
 import mechanicalsoup
-import re
+#import re
+#import time
+#import zlib
+from urllib.parse import unquote
 
 sg_home = 'https://www.sugar-garden.org'
 sg_login = '/login'
 sg_search = '/dashboard/search'
-sg_email = 'eddyden67@yahoo.com.tw'
-sg_pwd = 'jan237'
+sg_search_data = '/getSearchData'
 
 def login_sugar(browser):
-    login_url = sg_home + sg_login
-    
+    login_url = sg_home + sg_login    
     page = browser.get(login_url)
+    
+    cfp_url = sg_home + '/cfp'
+    cfp = browser.get(cfp_url)
+
+    with open('sg.txt', mode='r') as sg:
+        my = sg.readlines()
+        sg_email = my[0].rstrip('\n')
+        sg_pwd = my[1].rstrip('\n')
+    
     form = page.soup.find('form')
     fields = form.findAll('input')
     formdata = dict( (field.get('name'), field.get('value')) for field in fields )
     formdata['email'] = sg_email
     formdata['password'] = sg_pwd
     page = browser.post(login_url, data=formdata)
-    return page
-
-def search_sugar(browser, agefrom = 18, ageto = 26, seqtime = 1):
+    
+    formdata.pop('email')
+    formdata.pop('password')
+    return page, cfp, formdata
+'''
+def search_sugar(browser, cfp, formdata, agefrom = 18, ageto = 26, seqtime = 1):
     search_url = sg_home + sg_search
     page = browser.get(search_url)
-    form = page.soup.find('form')
-    fields = form.findAll('input')
-    formdata = dict( (field.get('name'), field.get('value')) for field in fields )
-    formdata['county'] = '臺北市'
-    formdata['agefrom'] = int(agefrom)
-    formdata['ageto'] = int(ageto)
-    formdata['seqtime'] = int(seqtime)
-    page = browser.post(search_url, data=formdata)
-    return page
-
-def view_sugar(browser, viewuser):
-    view_url = sg_home + viewuser
-    page = browser.get(view_url)
-    return page
-
+    
+    token = formdata['_token']
+    token_time = round(time.time())
+    cfp_url = sg_home + '/check-cfp?' + token + '=' + str(token_time)
+    check_cfp = browser.post(cfp_url, data=formdata)
+    return page, check_cfp
+'''
 def parse_search(browser):
-    page = search_sugar(browser)
+    cookies = browser.session.cookies.items()
+    data_url = sg_home + sg_search_data
+    data_header = {
+        'Content-Type': 'application/json',
+        'X-XSRF-TOKEN': unquote(cookies[0][1])
+        }
+    file_data = open('file.txt', mode='rb')
+    page = browser.request(method='POST', url=data_url, headers=data_header, data=file_data)
     
-    li_list = page.soup.find_all('li', class_='nt_fg')
-    sugar_list = []
-    for li in li_list:
-        tagText = li.find_all('div', class_='tagText')
-        link = li.find('a')['href']
-        img = li.find('img', class_='lazy')['src']
-        h2 = li.find('h2').strings
-        name_age = []
-        for s in h2:
-            name_age.append(s)
-        h3 = li.find_all('h3')
-        for s in h3[0].strings:
-            city = s.strip()
-            break
-        last_online = h3[1].text
-        sugar = [tagText, link, img, name_age, city, last_online]
-        sugar_list.append(sugar)
-        #print(sugar)
-    return sugar_list
+    sugar_data = page.json()
+    sugar_total = sugar_data['allPageDataCount']
+    sugar_list = sugar_data['dataList']
+    return sugar_total, sugar_list
 
-def parse_sugar(browser, link):
-    page = view_sugar(browser, link)
-    print(page)
-    
-    d1 = page.soup.find('div', class_='swiper-wrapper')
-    img_list = [img['src'] for img in d1.find_all('img')]
-    d2 = page.soup.find('div', class_='xl_input')
-    info = [d.text for d in d2.find_all('div', class_=re.compile('select_xx'))]
+def print_to_file(page):
+    with open('page.txt', mode='w') as file_object:
+        print(page.soup, file=file_object)
 
 def run():
     browser = mechanicalsoup.StatefulBrowser()
-    page = login_sugar(browser)
-    sugar_list = parse_search(browser)
+    
+    page, cfp, formdata = login_sugar(browser)
+    #print_to_file(page)
+    
+    #formdata['_token'] = 'eoIvrfOu2KjcEEWvUGbEyh2RT16Yty8yBNzanBSs'
+    #formdata['hash'] = 'fUihTT3jsiU8sediyzRXUxz1zewKpr7wkWThAtsYDsOcRZdn4t'
+    #page, check_cfp = search_sugar(browser, cfp, formdata)
+    #print_to_file(page)
+    
+    sugar_total, sugar_list = parse_search(browser)
     
     sugar = sugar_list[0]
-    parse_sugar(browser, sugar[1])
 
 #print( run() )
